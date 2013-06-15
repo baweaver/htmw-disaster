c4957b68b78d7752a2dbf722cf3cc03af36eb0dd
Brandon Weaver <lemur@Jinn.(none)>
2013-06-15 11:27:25 -0500

    Initial Commit, Rails generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HtmwDisaster::Application.config.secret_token = '97f46a6cb21ca7198553b0d415d3c7f8b920fe4e48c5bce1855dca0de6dc8d4972bbd4f180586fa49ac8c122ff50cf2e772467f7161f717a752c3a158afb0a2e'
