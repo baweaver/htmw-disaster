e5b464286d8e48af180f63dee9edfc524ee475df
Eric Poe <eric.poe@gmail.com>
2013-06-16 03:17:16 -0500

    Converted index.html to HAML and created controller/view for it as well as made it so it will be seen as the index for the site.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 HtmwDisaster::Application.routes.draw do
+  get "home/index"
+
   get "flare/list"
 
   get "categories/read"
@@ -66,7 +68,7 @@ HtmwDisaster::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
