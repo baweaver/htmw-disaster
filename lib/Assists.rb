f8b9af953adb76441698dc032262ec49c5a1c657
william <willis127@gmail.com>
2013-06-16 00:06:14 -0500

    access database methods

---
+++ lib/Assists.rb
@@ -0,0 +1,32 @@
+require 'sqlite3'
+require 'time'
+
+class Assists
+
+def dbCon
+	db = SQLite3::Database.open "../db/development.sqlite3"
+	return db
+	end
+	
+def initailize
+
+end
+	
+def updateFlare(flare_ID="null")
+	db = dbCon
+	t = Time.now
+	tt = Time.parse(t.to_s)
+	db.execute('INSERT into ASSISTS values (null,'+ flare_ID +',null,'+ tt +')')
+	db.close
+	end
+
+def updateResponder(flare_ID="null", responder_ID="null")
+	db = dbCon
+	db.execute('update ASSISTS set RESPONDER_ID=? WHERE FLARE_ID='+ flare_ID +')', responder_ID)
+	db.close
+end
+
+
+
+
+end
---
+++ lib/Flare.rb
@@ -0,0 +1,68 @@
+require 'sqlite3'
+
+class Flare
+
+def dbCon
+	db = SQLite3::Database.open "../db/development.sqlite3"
+	return db
+	end
+	
+def initailize
+	db = dbCon
+	db.execute('INSERT into FLARES values (null,"null","null","null", "null","null","0","null","null")')
+	db.close
+end
+	
+def updatePhone(srcPhone="null")
+	db = dbCon
+	db.execute('INSERT into FLARES values (null,' + srcPhone + ',"null","null", "null","null","0","null","null")')
+	db.close
+	end
+
+def updateZip(srcPhone="null", zip="null")
+	db = dbCon
+	db.execute('update FLARES set ZIP=? WHERE FLARE_ID=(SELECT MAX(FLARE_ID) FROM FLARES WHERE SRCPHONE='+ srcPhone +')', zip)
+	db.close
+end
+
+def updateLocation(srcPhone="null", loc="null")
+	db = dbCon
+	db.execute('update FLARES set LOCATION=? WHERE FLARE_ID=(SELECT MAX(FLARE_ID) FROM FLARES WHERE SRCPHONE='+ srcPhone +')', loc)
+	db.close
+end
+
+def updateCategory(srcPhone="null", category="null")
+	db = dbCon
+	db.execute('update FLARES set CATEGORY=? WHERE FLARE_ID=(SELECT MAX(FLARE_ID) FROM FLARES WHERE SRCPHONE='+ srcPhone +')', category)
+	db.close
+end
+
+def updateDescription(srcPhone="null", description="null")
+	db = dbCon
+	db.execute('update FLARES set DESCRIPTION=? WHERE FLARE_ID=(SELECT MAX(FLARE_ID) FROM FLARES WHERE SRCPHONE='+ srcPhone +')', description)
+	db.execute('update FLARES set ACTIVE=? WHERE FLARE_ID=(SELECT MAX(FLARE_ID) FROM FLARES WHERE SRCPHONE='+ srcPhone +')', 1)
+	db.close
+end
+
+def updateActive(srcPhone="null", active="null")
+	db = dbCon
+	db.execute('update FLARES set ACTIVE=? WHERE FLARE_ID=(SELECT MAX(FLARE_ID) FROM FLARES WHERE SRCPHONE='+ srcPhone +')', active)
+	db.close
+end
+
+def updateCreatedDate(srcPhone="null", date="null")
+	db = dbCon
+	db.execute('update FLARES set CREATED_DT=? WHERE FLARE_ID=(SELECT MAX(FLARE_ID) FROM FLARES WHERE SRCPHONE='+ srcPhone +')', date)
+	db.close
+end
+
+def updateDate(srcPhone="null", date="null")
+	db = dbCon
+	db.execute('update FLARES set UPDATED_DT=? WHERE FLARE_ID=(SELECT MAX(FLARE_ID) FROM FLARES WHERE SRCPHONE='+ srcPhone +')', date)
+	db.close
+end
+
+
+
+
+end
